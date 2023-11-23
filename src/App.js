7efcee4d9ea5a9d41b411aec710cdf15104057c9
mooncoior <mooncoior77@gmail.com>
2023-11-23 09:50:55 +0900

    리액트 수업 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './test/Welcome';
+import { Route,Routes } from "react-router-dom" 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/test/welcome" element={<Welcome/>}/>
+      {/* <div class="main">
+		<form name="loginForm">
+			<table>
+        <div class="title">서비스 이용을 위해<br/>로그인을 해주세요.</div>
+				<tr>
+					<td>
+						<input type="text" name="mid" placeholder="아이디" class="mid"/>
+					</td> 
+				</tr> 
+				<tr>
+					<td>
+						<input type="password" name="pwd" class="pwd" placeholder="비밀번호"/>
+					</td> 
+				</tr> 
+			</table>
+      <div class="auto_box">
+        <input type="checkbox" name="autoLogin" value="yes" class="autoLogin"/>
+        <span class="autoTxt">자동 로그인</span>
+      </div>
+      <div class="button_box">
+        <input type="button" value="Login" class="loginBtn" onClick="checkLoginForm()"/>
+      
+      </div>
+		</form>
+  </div> */}
+    </Routes>
+    </>
+    
+  
   );
 }
 
---
+++ src/test/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+       <div class="main">
+		<form name="loginForm">
+        <div class="title"><h1>프?로젝트</h1></div>
+		</form>
+  </div>
+    );
+  }
+  
+  export default Welcome;
